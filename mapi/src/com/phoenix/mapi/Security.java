a7ba59c662076813196c02b65dda5ee8635a5d84
skiller1 <stevewu1@outlook.com>
2012-09-24 22:12:40 -0700

    newer

---
+++ mapi/src/com/phoenix/mapi/Security.java
@@ -0,0 +1,15 @@
+package com.phoenix.mapi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Security extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+	}
+	
+}
