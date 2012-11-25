84169e074994505f62719b5e92372a599e93c7e4
yhnehc <yhnehc@gmail.com>
2012-11-25 12:25:00 -0800

    new commit

---
+++ DashboardActivity/src/com/example/dashboardactivity/customMarkerOverlay.java
@@ -0,0 +1,51 @@
+package com.example.dashboardactivity;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class customMarkerOverlay extends ItemizedOverlay{
+
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	Context mContext;
+	public customMarkerOverlay(Drawable defaultMarker) {
+		super(boundCenterBottom(defaultMarker));
+	}
+
+	@Override
+	protected OverlayItem createItem(int i) {
+		  return mOverlays.get(i);
+		}
+
+	@Override
+	public int size() {
+		  return mOverlays.size();
+		}
+	
+	public void addOverlay(OverlayItem overlay) {
+	    mOverlays.add(overlay);
+	    populate();
+	}
+
+	public customMarkerOverlay(Drawable defaultMarker, Context context) {
+		  super(boundCenterBottom(defaultMarker));
+		  mContext = context;
+		}
+
+	@Override
+	protected boolean onTap(int index) {
+	  OverlayItem item = mOverlays.get(index);
+	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+	  dialog.setTitle(item.getTitle());
+	  dialog.setMessage(item.getSnippet());
+	  dialog.show();
+	  return true;
+	}
+
+}
+
